7a1f1d4bbe9d74ad86595d47521e2371380eb8eb
jeaved08 <51840971+jeaved08@users.noreply.github.com>
2019-06-15 14:34:24 +0300

    Add files via upload

---
+++ gallows.py
@@ -0,0 +1,32 @@
+#Виселица
+import random
+
+#Функция ответственная за рандомное слово
+def random_word():
+    x = random.choice([
+    "apple", 
+    "lamon", 
+    "man",
+    "woman",
+    "friend", 
+    "businessman", 
+    "doctor",
+    "artist",
+    "cat", 
+    "dog",
+    "wolf", 
+    "house", 
+    "tree", 
+    "room"])
+    return x
+choice = random_word()
+print("Слово загаданно")
+
+#Длина слова
+lenght = len(choice("-"))
+print("Длина слова : ", lenght)
+
+#Функция определяющая жизни при отгадывании
+def life_in_the_game():
+    litter = input("Введите букву : ")
+    if litter == choice:
